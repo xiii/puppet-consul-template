4560410e980d8cc2e16ffa26f903f4f79350de35
Efstathios Xagoraris <efstathios.xagoraris@itv.com>
2017-02-08 09:05:20 +0000

    lint

--- localmodules/profile/manifests/publisher.pp
+++ localmodules/profile/manifests/publisher.pp
@@ -1,20 +1,20 @@
 class profile::publisher {
 
-  $description = hiera("description")
+  $description = hiera('description')
 
-  notice("------------------------------------------------------")
-  notice("Description: $description")
-  notice("To SSH: vagrant ssh $::role")
-  notice("IP: $::ipaddress_enp0s8")
+  notice('------------------------------------------------------')
+  notice("Description: ${description}")
+  notice("To SSH: vagrant ssh ${::role}")
+  notice("IP: ${::ipaddress_enp0s8}")
   notice("RabbitMQ admin interface: http://${::ipaddress_enp0s8}:15672")
-  notice("------------------------------------------------------")
+  notice('------------------------------------------------------')
 
   class { '::epel': }
-   ->
+  ->
   class { '::erlang': }
-   ->
+  ->
   class { '::rabbitmq':
-     package_ensure              => "3.3.5-28.el7",
- }
- 
+    package_ensure              => '3.3.5-28.el7',
+  }
+
 }
